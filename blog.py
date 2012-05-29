2b2844a6bdb1f8fa00ac5b5fd7ec129ff953b68a
Scott Robert Lawrence <theelven@gmail.com>
2012-05-28 23:10:59 -0700

    Added better post formatting, last_modified

--- blog.py
+++ blog.py
@@ -1,9 +1,17 @@
 from GAEblog import *
 
+def blog_key(name = 'default'):
+	return db.Key.from_path('blogs', name)
+
 class Post(db.Model):
 	subject = db.StringProperty(required = True)
 	content = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
+	last_modified = db.DateTimeProperty(auto_now = True)
+	
+	def render(self):
+		self._render_text = self.content.replace('\n', '<br>')
+		return render_str("post.html", p = self)
 
 class Blog(Handler):
 	def render_front(self, error=""):
